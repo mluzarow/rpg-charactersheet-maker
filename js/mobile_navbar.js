47fc5a7a12edc8a06e3944541c634bb7985f69eb
Mackenzie <ma.luzarow@gmail.com>
2017-12-23 09:53:01 -0600

    Init commit.

---
+++ js/mobile_navbar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function navbarMobileOpen () {
+    document.getElementById("navbar_mobile_frame_side").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function navbarMobileClose () {
+    document.getElementById("navbar_mobile_frame_side").style.width = "0";
+}
